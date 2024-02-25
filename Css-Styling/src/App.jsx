d28c7df6b7df4583d213e2444b8669e8b48a409a
Cameron Kelly <cameron.kelly.0316@gmail.com>
2024-02-25 01:22:21 -0500

    First Commit

---
+++ Css-Styling/src/App.jsx
@@ -0,0 +1,12 @@
+//HOW TO STYLE REACT COMPONENTS WITH CSS
+// EXTERNAL
+//MODULES
+//INLINE
+import Button from "./Button.jsx"
+
+const App =() =>{
+  return(
+  <Button />
+  );
+}
+export default App
